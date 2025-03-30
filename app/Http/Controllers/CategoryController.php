867d7fbaac383454b8f4ce110ff0620b2e58cef9
Diana Perez <dianaperezc10@gmail.com>
2025-03-30 13:16:53 -0500

    -.- modificacion de la funcion destroy -_-

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -69,10 +69,13 @@ class CategoryController extends Controller
     }
 
     /**
-     * Remove the specified resource from storage.
+     * @param int $id
+     * @return \Illuminate\Http\Response
      */
     public function destroy(string $id)
     {
-        //
+        $categories = Category::findOrFail($id);
+        $categories->delete();
+        return redirect()->route('categories.index');
     }
 }
